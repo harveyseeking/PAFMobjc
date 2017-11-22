c12c7f7c0c6a2d2715542420d45aa25efa86629c
高立志(金融壹账通科技中心银行实施团队前端组) <gaolizhi893@pingan.com.cn>
2017-11-22 17:31:13 +0800

    [富民DEMO][主工程][M][C:高立志]创建主工程

---
+++ PAFMiOSDEMO/PAFMiOSDEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PAFMiOSDEMO
+//
+//  Created by paglz on 2017/11/22.
+//  Copyright © 2017年 PAFM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
